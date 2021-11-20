edb7ba5f74e7827c95a5e4c94131f13fb6d981eb
Bologov <bologov@macroscop.com>
2021-11-21 03:19:26 +0500

    Подключение к БД при старте приложения

--- Enaza/Enaza/Startup.cs
+++ Enaza/Enaza/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Enaza.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Enaza
 {
@@ -26,6 +21,11 @@ namespace Enaza
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
+			services.AddDbContext<UsersContext>(options =>
+			{
+				var connectionString = Configuration.GetConnectionString("DbConnection");
+				options.UseSqlServer(connectionString);
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
