9b29109b285aca38d1784cb7aa5a4e21c3b0d367
Bologov <bologov@macroscop.com>
2021-11-21 03:19:02 +0500

    Создан UsersContext

---
+++ Enaza/Enaza/Contexts/UsersContext.cs
@@ -0,0 +1,17 @@
+﻿using Enaza.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Enaza.Contexts
+{
+	public sealed class UsersContext : DbContext
+	{
+		public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+		{
+			Database.EnsureCreated();
+		}
+
+		public DbSet<UserModel> Users { get; set; }
+		public DbSet<UserGroupModel> UserGroups { get; set; }
+		public DbSet<UserStateModel> UserStates { get; set; }
+	}
+}
