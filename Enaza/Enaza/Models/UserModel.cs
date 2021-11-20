203efeda0a0fc48139fb30f53f42ed322ce628e9
Bologov <bologov@macroscop.com>
2021-11-21 01:55:10 +0500

    Модели для БД

---
+++ Enaza/Enaza/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Enaza.Models
+{
+	public class UserModel
+	{
+		public int UserId { get; set; }
+		public string Login { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public int UserGroupId { get; set; }
+		public int UserStateId { get; set; }
+	}
+}
