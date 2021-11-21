0900c629af2d1bf83cb2600d92a3d64c6fcc29d8
Bologov <bologov@macroscop.com>
2021-11-22 00:52:54 +0500

    Миграция для добавления записей со статусами пользователей

---
+++ Enaza/Enaza/Migrations/20211121185523_InitialUserStates.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Enaza.Migrations
+{
+    public partial class InitialUserStates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "UserStates",
+                columns: new[] {"UserStateId", "Code", "Description"},
+                values: new object[,]
+                {
+                    {0, "Active", "User is active and can be accessed."},
+                    {1, "Blocked", "User is deleted or blocked."}
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
