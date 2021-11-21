f15554712f342cdf8c153120da253b5657d6ea23
Bologov <bologov@macroscop.com>
2021-11-21 20:51:07 +0500

    Интерфейс CRUDA над пользователями (сервис и репозиторий)

---
+++ Enaza/Enaza/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Enaza.Models;
+
+namespace Enaza.Repositories
+{
+	public interface IUserRepository
+	{
+		Task<List<UserModel>> GetAllUsers();
+		Task<UserModel> GetUserById(int id);
+		Task<UserModel> AddUser(UserModel user);
+		Task<UserModel> MarkUserAsBlocked(int id);
+		Task<UserModel> GetUserByLogin(int id);
+	}
+}
---
+++ Enaza/Enaza/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Enaza.Models;
+
+namespace Enaza.Services
+{
+	public interface IUserService
+	{
+		Task<List<UserModel>> GetAllUsers();
+		Task<UserModel> GetUser(int userId);
+		Task<UserModel> AddUser(string login, string password);
+		Task DeleteUser(int userId);
+	}
+}
