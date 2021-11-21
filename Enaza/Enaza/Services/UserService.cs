6e58197ab8d6c0e1dac4c8e85e31c6d2cfbb8030
Bologov <bologov@macroscop.com>
2021-11-21 21:50:49 +0500

    Реализация сервиса пользователей

---
+++ Enaza/Enaza/Services/UserService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Enaza.Models;
+using Enaza.Repositories;
+
+namespace Enaza.Services
+{
+	public class UserService : IUserService
+	{
+		private readonly IUserRepository _userRepository;
+
+		public UserService(IUserRepository userRepository)
+		{
+			_userRepository = userRepository;
+		}
+
+		public async Task<List<UserModel>> GetAllUsers()
+		{
+			var users = await _userRepository.GetAllUsers();
+			return users;
+		}
+
+		public async Task<UserModel> GetUser(int userId)
+		{
+			var user = await _userRepository.GetUserById(userId);
+			return user;
+		}
+
+		public async Task<UserModel> AddUser(string login, string password)
+		{
+			var user = new UserModel
+			{
+				Login = login,
+				Password = password,
+				UserGroupId = 0,
+				UserStateId = 0
+			};
+
+			var addedUser = await _userRepository.AddUser(user);
+			return addedUser;
+		}
+
+		public async Task DeleteUser(int userId)
+		{
+			await _userRepository.MarkUserAsBlocked(userId);
+		}
+	}
+}
