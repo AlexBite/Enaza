e811e8634ab5ecfb4b35142c41e5209a9448e945
Bologov <bologov@macroscop.com>
2021-11-22 00:52:35 +0500

    Миграция для добавления записей с группами пользователей

---
+++ Enaza/Enaza/Migrations/20211121181953_InitialUserGroups.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Enaza.Migrations
+{
+    public partial class InitialUserGroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "UserGroups",
+                columns: new[] {"UserGroupId", "Code", "Description"},
+                values: new object[,]
+                {
+                    {0, "Admin", "Administrator user. Only one in the system. Can add or delete users."},
+                    {1, "User", "Just a user. Do not have rights to add or delete users."}
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
