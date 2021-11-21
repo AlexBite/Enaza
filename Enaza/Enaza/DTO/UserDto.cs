0aeee079b691f9e2f8eb746f2e9925d5a5ef1fd4
Bologov <bologov@macroscop.com>
2021-11-21 20:51:32 +0500

    DTO пользователя

---
+++ Enaza/Enaza/DTO/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Enaza.DTO
+{
+	public class UserDto
+	{
+		public int Id { get; set; }
+		public string Login { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public UserGroupDto UserGroup { get; set; }
+		public UserStateDto UserState { get; set; }
+	}
+}
---
+++ Enaza/Enaza/DTO/UserGroupDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Enaza.DTO
+{
+	public class UserGroupDto
+	{
+		public int Id { get; set; }
+		public string Code { get; set; }
+		public string Description { get; set; }
+	}
+}
